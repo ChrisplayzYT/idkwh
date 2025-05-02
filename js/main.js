7f3c9476187fc039458a7926038626fed1e8594d
ChrisplayzWii <191655784+ChrisplayzWii@users.noreply.github.com>
2025-05-02 11:10:40 -0500

    ok

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ function changelink(id) {
 }
 
 function copy() {
-    const text = "idkwhereisthisname";
+    const text = "https://idkwhereisthis.name";
     let alertMsg = "username successfully copied to your clipboard!";
     let errMsg = "failed to copy to clipboard, please try again";
 
@@ -21,26 +21,6 @@ class Main {
     constructor() { };
 }
 
-function setCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
-}
-
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-}
 
 class webSound {
     constructor() {
